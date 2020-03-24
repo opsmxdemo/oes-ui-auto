478a206a425e79a4901be675887d8b73710df5ad
Siddhu <rajinderpalsiddhu@gmail.com>
2020-03-24 18:22:17 +0530

    Initial commit related to base structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import * as fromApp from './store/app.reducer';
+import { AppRoutingModule } from './subModules/app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { AppMaterialModule } from './subModules/app-material.module';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    AppMaterialModule,
+    StoreModule.forRoot(fromApp.appReducers),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
