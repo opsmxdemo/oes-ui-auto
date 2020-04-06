6a1ad2d043ea22c909026feeae64aec0b4e3daab
Siddhu <rajinderpalsiddhu@gmail.com>
2020-04-06 13:49:17 +0530

    feature/Audit: Changes related to dynamic table

--- src/app/services/audit.service.ts
+++ src/app/services/audit.service.ts
@@ -14,9 +14,7 @@ getAllPipelines(parameters) {
     const params  =  new HttpParams()
         .set('isLatest','false')
         .set('authToken','SESSION=ZTNiZTc4OWItMTc0Ni00M2NiLWE5Y2QtNTJmNmMyYzBkYWU3');
-    return this.httpClient.get('http://34.83.55.40:8050/audit/getAllDeployments',{
-        params :params
-    });
+    return this.httpClient.get('https://137.117.94.95:8050/audit/getAllDeployments?isLatest=false&authToken=SESSION=YzFlY2MzZDYtY2ZhOC00NjU4LTk2Y2ItY2YwMmE3ZTExYWY5&isTreeView=true');
 }
 
 }
