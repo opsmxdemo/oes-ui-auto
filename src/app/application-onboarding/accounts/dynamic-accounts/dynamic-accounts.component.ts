ef92d025a45565619689066c983893e6ea93c79e
vvtelu <satya@opsmx.io>
2020-05-04 17:49:16 +0530

    created component for dynamic accounts in setting up environment

---
+++ src/app/application-onboarding/accounts/dynamic-accounts/dynamic-accounts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-accounts',
+  templateUrl: './dynamic-accounts.component.html',
+  styleUrls: ['./dynamic-accounts.component.less']
+})
+export class DynamicAccountsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
