f2acf7b324dde4025e46685fd2b5ed38f1e872d1
Satyan <satya@opsmx.io>
2020-06-23 14:53:03 +0530

    1. Done with the integration Application and services view 2. Integrated Listing service

--- src/app/application/deployment-verification/store/deploymentverification.reducer.ts
+++ src/app/application/deployment-verification/store/deploymentverification.reducer.ts
@@ -1,16 +1,37 @@
 import { Action, createReducer, on } from '@ngrx/store';
 import * as DeploymentActions from './deploymentverification.actions';
+import { Observable } from 'rxjs';
+import { User } from '../deployment-verification.component';
 
 export interface State {
     canaryRun: any;
     errorMessage: string;
     deployementLoading: boolean;
+    canaryId: any;
+    // variables to store application details
+    applicationList: User[];
+    applicationListLoading: boolean;
+    serviceList: [];
+    serviceListLoading: boolean;
+    applicationHealthDetails: [];
+    applicationHealthDetailsLoading: boolean;
+    serviceInformation: any;
+    serviceInformationLoading: boolean;
 }
 
 export const initialState: State = {
     canaryRun: null,
     errorMessage: null,
-    deployementLoading: false
+    deployementLoading: false,
+    applicationList: null,
+    applicationListLoading: false,
+    serviceList: null,
+    serviceListLoading: false,
+    canaryId: null,
+    applicationHealthDetails: null,
+    applicationHealthDetailsLoading: false,
+    serviceInformation: null,
+    serviceInformationLoading: false,
 }
 
 export function DeploymentdReducer(
@@ -36,6 +57,60 @@ export function DeploymentdReducer(
                 ...state,
                 errorMessage: action.errorMessage,
                 deployementLoading: false
+            })
+        ),
+        on(DeploymentActions.loadApplications,
+            state => ({
+                ...state,
+                applicationListLoading: true
+            })
+        ),
+        on(DeploymentActions.fetchApplications,
+            (state,action) => ({
+                ...state,
+                applicationList: action.applicationList,
+                applicationListLoading:false
+            })
+        ),
+        on(DeploymentActions.loadServices,
+            (state,action) => ({
+                ...state,
+                canaryId: action.canaryId,
+                serviceListLoading: true
+            })
+        ),
+        on(DeploymentActions.fetchServices,
+            (state,action) => ({
+                ...state,
+                serviceList: action.servicesList,
+                serviceListLoading:false
+            })
+        ),
+        on(DeploymentActions.loadApplicationHelath,
+            (state,action) => ({
+                ...state,
+                canaryId: action.canaryId,
+            })
+        ),
+        on(DeploymentActions.fetchApplicationHelath,
+            (state,action) => ({
+                ...state,
+                applicationHealthDetails: action.applicationHealthDetails,
+                applicationHealthDetailsLoading:false
+            })
+        ),
+        on(DeploymentActions.loadServiceInformation,
+            (state,action) => ({
+                ...state,
+                canaryId: action.canaryId,
+                serviceId: action.serviceId,
+            })
+        ),
+        on(DeploymentActions.fetchServiceInformation,
+            (state,action) => ({
+                ...state,
+                serviceInformation: action.serviceSummary,
+                serviceInformationLoading:false
             })
         ),
     )(deploymentVerificationState,deploymentVerificationdActions);
