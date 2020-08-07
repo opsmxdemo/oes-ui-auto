60c292fe0be672cf3062b9b1d6cc17a30aa2f980
vvtelu <satya@opsmx.com>
2020-08-07 17:11:51 +0530

    endpoint changes for login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,7 +32,7 @@ export class AppComponent implements OnInit, AfterViewChecked {
               private router: Router,
               private route: ActivatedRoute,
               public environment: AppConfigService) {
-                this.endpointUrl = environment.config.uiEndPoint;
+                this.endpointUrl = environment.config.endPointUrl;
                }
   // For tooltip
   ngAfterViewChecked() {
