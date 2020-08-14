ec17a459a6776f08a39220b47b7324fadd4cda23
Siddhu <rajinderpalsiddhu@gmail.com>
2020-08-14 21:38:06 +0530

    OP-3071: Created separate store for accounts component also same as application component in setup page.

---
+++ src/app/application-onboarding/accounts/store/accounts.actions.ts
@@ -0,0 +1,16 @@
+import { createAction, props } from '@ngrx/store';
+
+// Below action related to Account
+export const loadAccount = createAction('[Accounts] LoadAccount' , props<{page:string}>());
+export const createAccount = createAction('[Accounts] createAccount', props<{accountData:any,postData:any}>())
+export const updateAccount = createAction('[Accounts] updateAccount', props<{accountData:any,postData:any}>())
+export const accountDataSaved = createAction('[Accounts] AccountDataSaved');
+export const errorOccured = createAction('[Accounts] ErrorOccured', props<{errorMessage:string}>());
+
+// Below action is related to accounts list
+export const loadAccountList = createAction('[Accounts] LoadAccountList');
+export const fetchAccountList = createAction('[Accounts] FetchAccountList', props<{Accountlist: any}>());
+export const deleteAccount = createAction('[Accounts] DeleteAccount', props<{accountName:any,index:number}>())
+export const accountDeleted = createAction('[Accounts] AccountDeleted', props<{index:number}>());
+//export const accountDeletedSuccessfully = createAction('[Accounts] AccountDeletedSuccessfully', props<{index:number}>());
+
