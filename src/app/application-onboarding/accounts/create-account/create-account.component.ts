27d3375d048cedb4a74cb2dc86b747df8d39f94f
Siddhu <rajinderpalsiddhu@gmail.com>
2020-08-14 21:32:48 +0530

    OP-3071: Created separate accounts component and put related component in it . i.e, Create account and dynamic accounts.

---
+++ src/app/application-onboarding/accounts/create-account/create-account.component.ts
@@ -0,0 +1,132 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
+import { SharedService } from 'src/app/services/shared.service';
+import { Store } from '@ngrx/store';
+import * as fromFeature from '../../store/feature.reducer';
+import { CreateAccount } from 'src/app/models/applicationOnboarding/createAccountModel/createAccount.model';
+import * as AccountActions from '../store/accounts.actions';
+import { Router } from '@angular/router';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-create-account',
+  templateUrl: './create-account.component.html',
+  styleUrls: ['./create-account.component.less']
+})
+export class CreateAccountComponent implements OnInit {
+  
+ // createAccountForm: FormGroup;                               // Form for Account details      
+  mainForm: CreateAccount = null;                             // It contain data form which send to backend after successful submission.                                                                
+  accountData: CreateAccount = null;
+  parentRedirection: string;
+  namespacesList: [];
+  readList: [];
+  writeList: [];
+  executeList: []; 
+  postDataForm: any;
+  fileContent:any;
+
+  createAccountForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    accountType: new FormControl('',[Validators.required]),
+    namespaces: new FormControl('',[Validators.required]),
+    read: new FormControl('',[Validators.required]),
+    write: new FormControl('',[Validators.required]),
+    execute: new FormControl('',[Validators.required]),
+    file: new FormControl('', [Validators.required]),
+    fileSource: new FormControl('', [Validators.required])
+  });
+
+
+  constructor(public sharedService: SharedService,
+    public store: Store<fromFeature.State>,
+              public router: Router) { }
+
+  ngOnInit(): void {
+    this.store.select(fromFeature.selectAccounts).subscribe(
+      (response)=> {
+        if(response.accountParentPage){
+          this.parentRedirection = response.accountParentPage;
+        }
+      }
+    )
+
+    if(this.sharedService.getAccountType() === 'editAcc'){
+      this.createAccountForm.patchValue({
+        name: this.sharedService.getUserData().name,
+        accountType: 'Kubernetes',
+        namespaces: this.sharedService.getUserData().namespaces,
+        read: this.sharedService.getUserData().permissions.READ,
+        write: this.sharedService.getUserData().permissions.WRITE,
+        execute: this.sharedService.getUserData().permissions.EXECUTE,
+        //file: this.sharedService.getUserData().kubeconfigFile,
+      });
+    }
+   
+  }
+
+  get accountform(){
+    return this.createAccountForm.controls;
+  }
+
+  onFileChange(event) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.fileContent = file;
+      this.createAccountForm.patchValue({
+        fileSource: file
+      });
+    }
+  }
+
+  // account form validations
+
+  validate(){
+    this.createAccountForm.markAllAsTouched();
+    return this.createAccountForm.valid;
+  }  
+  
+  //Below function is use to submit whole form and send request to backend
+
+  submitForm(data){
+   if(this.sharedService.getAccountType() === 'editAcc'){
+    this.namespacesList = data.namespaces;
+    this.readList = data.read;
+    this.writeList = data.write;
+    this.executeList = data.execute;
+   }else{
+    this.namespacesList = data.namespaces.split(",");
+    this.readList = data.read.split(",");
+    this.writeList = data.write.split(",");
+    this.executeList = data.execute.split(",");
+   }
+  
+    
+    this.postDataForm = {
+      name : data.name,
+      accountType : data.accountType,
+      namespaces : this.namespacesList,
+      read : this.readList,
+      write : this.writeList,
+      execute : this.executeList
+      }
+     
+    const formData = new FormData();
+    formData.append('files', this.fileContent,'kubeconfig');
+
+    if(this.sharedService.getAccountType() === 'editAcc'){
+      this.store.dispatch(AccountActions.createAccount({accountData: formData,postData: JSON.stringify(this.postDataForm)}));
+    }else{
+     this.store.dispatch(AccountActions.createAccount({accountData: formData,postData:JSON.stringify(this.postDataForm)}));
+    }
+  }
+
+
+
+  // Below function is use to redirect to parent page after click on cancel btn
+  cancelForm(){
+    this.createAccountForm.reset();
+    this.router.navigate([this.parentRedirection]);
+  }
+
+}
---
+++ src/app/application-onboarding/accounts/dynamic-accounts/dynamic-accounts.component.ts
@@ -0,0 +1,177 @@
+import { Component, OnInit } from '@angular/core';
+import * as fromFeature from '../../store/feature.reducer';
+import * as AccountsActions from '../store/accounts.actions';
+import { Store } from '@ngrx/store';
+import * as $ from 'jquery';
+import { NotificationService } from 'src/app/services/notification.service';
+import { SharedService } from 'src/app/services/shared.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-dynamic-accounts',
+  templateUrl: './dynamic-accounts.component.html',
+  styleUrls: ['./dynamic-accounts.component.less']
+})
+export class DynamicAccountsComponent implements OnInit {
+  tableIsEmpty: boolean = false;                                                       // It use to hide table if no record exist in it.
+  accountListData: any;                                                                // It use to store Accountlist data fetched from state.
+  searchData: string = '';                                                             // this is use to fetch value from search field.
+  perPageData: number = 10;                                                            // this is use to populate value in perPage dropdown exist in pagination.
+  page = {                                                                             // this is use to support pagination in accunt page.
+    startingPoint: 0,
+    endPoint: 10,
+    pageSize: 10,
+    currentPage: 1,
+    pageNo: 1,
+  }
+  currentPage = [];                                                                    // this use to store array of data exists in current page.
+  accountListLength: number = null;
+  nameOfAccount: null;
+  constructor(public store: Store<fromFeature.State>, public notifications: NotificationService,
+              public sharedAccountData: SharedService) { }
+
+  ngOnInit(): void {
+    this.store.dispatch(AccountsActions.loadAccountList());
+
+     // fetching data from state
+     this.store.select(fromFeature.selectAccounts).subscribe(
+      (response) => {
+       if (response.accountList !== null) {
+        this.accountListData = response.accountList;
+        this.accountListLength = this.accountListData.length;
+        this.renderPage();
+        this.tableIsEmpty = false;
+      }else{
+        this.tableIsEmpty = true;
+      }
+      },
+      (error) => {
+        this.notifications.showError('Error',error);
+      }
+    );
+  }
+
+   // Below function is used if user want to refresh list data
+   refreshList(){
+    this.store.dispatch(AccountsActions.loadAccountList());
+  } 
+
+  //Below function is execute on search
+  onSearch(){
+    if(this.searchData !== ''){
+      this.currentPage = [];
+      for (let i = 0; i < this.accountListLength; i++) {
+        this.currentPage.push(this.accountListData[i]);
+      }
+    }else{
+      this.renderPage();
+    }
+  }
+
+  //Below function is used to implement pagination
+  renderPage() {
+    this.currentPage = [];
+    if(this.page.endPoint < this.accountListLength-1){
+      for (let i = this.page.startingPoint; i < this.page.endPoint; i++) {
+        this.currentPage.push(this.accountListData[i]);
+      }
+    }else{
+      for (let i = this.page.startingPoint; i < this.accountListLength; i++) {
+        this.currentPage.push(this.accountListData[i]);
+      }
+    }
+  }
+
+  //Below function is execute on change of perPage dropdown value
+  onChangePerPageData() {
+    this.page.pageSize = +this.perPageData;
+    if ((this.page.startingPoint + this.page.pageSize) < this.accountListLength) {
+      this.page.endPoint = this.page.startingPoint + this.page.pageSize;
+    } else {
+      this.page.endPoint = this.accountListLength;
+    }
+    this.renderPage();
+  }
+
+  //Below function is execute on click of page next btn
+  pageNext() {
+    if (this.page.endPoint < this.accountListLength-1) {
+      this.page.pageNo += 1;
+      this.page.currentPage = this.page.pageNo;
+      if ((this.page.endPoint + this.page.pageSize) < this.accountListLength) {
+        this.page.startingPoint += this.page.pageSize;
+        this.page.endPoint += this.page.pageSize;
+      } else if (this.page.endPoint < this.accountListLength) {
+        this.page.startingPoint = this.page.endPoint;
+        this.page.endPoint = this.accountListLength-1;
+      }
+      this.renderPage();
+    }
+  }
+
+  //Below function is execute on click of page prev btn
+  pagePrev() {
+    if (this.page.startingPoint !== 0) {
+      this.page.pageNo -= 1;
+      this.page.currentPage = this.page.pageNo;
+      if ((this.page.startingPoint - this.page.pageSize) > 0) {
+        this.page.startingPoint -= this.page.pageSize;
+        this.page.endPoint = this.page.startingPoint + this.page.pageSize;
+      } else if ((this.page.startingPoint - this.page.pageSize) >= 0) {
+        this.page.startingPoint = 0;
+        this.page.endPoint = this.page.pageSize;
+      }
+      this.renderPage();
+    }
+  }
+
+  //Below function is executes on click of page btn exist in pagination
+  showPage(currentPage) {
+    this.page.pageNo = currentPage;
+    this.page.startingPoint = (currentPage - 1) * this.page.pageSize;
+    if (currentPage * this.page.pageSize < this.accountListLength) {
+      this.page.endPoint = currentPage * this.page.pageSize;
+    } else {
+      this.page.endPoint = this.accountListLength-1;
+    }
+    this.renderPage();
+  }
+
+  // Below function is use to redirect to create account page
+  createAccount() {
+    $("[data-toggle='tooltip']").tooltip('hide');
+    this.sharedAccountData.setUserData([]);
+    this.store.dispatch(AccountsActions.loadAccount({page:'/setup/accounts'}));
+  }
+
+  // Below function is use to edit existing account
+  editAccount(data: any,type: string) {
+    $("[data-toggle='tooltip']").tooltip('hide');
+    //this.sharedAccountData.setUserData(data.name === 'kubernetes')
+    this.sharedAccountData.setUserData(data);
+    this.sharedAccountData.setAccountType(type);
+    this.store.dispatch(AccountsActions.loadAccount({page:'/setup/accounts'}));
+  }
+
+  // Below function is use to delete existiong account
+  deleteAccount(account: any,index) {
+    $("[data-toggle='tooltip']").tooltip('hide');
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    }).then((result) => {
+      if (result.value) {
+        $("[data-toggle='tooltip']").tooltip('hide');
+        this.store.dispatch(AccountsActions.deleteAccount({accountName: account.name,index:index}));
+      }else{
+        //alert('dont delete'); 
+      }
+    })
+  }
+
+}
