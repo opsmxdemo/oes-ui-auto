4109814d7bde76cf6e3d0788d8e45a5acc0c5670
Nitish-Dhawan <58160202+Nitish-Dhawan@users.noreply.github.com>
2020-09-22 11:24:34 +0530

    Update keys.pipe.ts

--- src/app/pipes/keys.pipe.ts
+++ src/app/pipes/keys.pipe.ts
@@ -64,15 +64,3 @@ export class ChangeTImestamptotimePipe {
   }
 }
 
-@Pipe({
-  name: 'callback',
-  pure: false
-})
-export class CallbackPipe implements PipeTransform {
-  transform(items: any[], callback: (item: any) => boolean): any {
-      if (!items || !callback) {
-          return items;
-      }
-      return items.filter(item => callback(item));
-  }
-}
