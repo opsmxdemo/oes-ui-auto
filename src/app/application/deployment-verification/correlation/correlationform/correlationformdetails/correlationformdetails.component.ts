3db5aa5ab15e5ba1410e2c46a0a2be0a407986d7
Nitish-Dhawane <nitish.dhawan@opsmx.io>
2020-10-07 18:34:02 +0530

    selectall changes

--- src/app/application/deployment-verification/correlation/correlationform/correlationformdetails/correlationformdetails.component.ts
+++ src/app/application/deployment-verification/correlation/correlationform/correlationformdetails/correlationformdetails.component.ts
@@ -21,19 +21,95 @@ export class CorrelationformdetailsComponent implements OnInit,OnChanges {
 
   constructor() { }
   ngOnChanges(changes: SimpleChanges): void {
-    this.modelCritical=false;
-    this.modelError=false;
-    this.modelWarn=false;
+    var criticalKeys = Object.keys(this.savedCheckedData.Critical)
+    var ErrorKeys = Object.keys(this.savedCheckedData.ERROR)
+    var WarningKeys = Object.keys(this.savedCheckedData.Warn)
+    
+    
+    for(var i=0;i< criticalKeys.length;i++)
+    {
+      if(this.savedCheckedData['Critical'][criticalKeys[i]]==false)
+      {
+        this.savedCheckedData['groupCritical']=false
+        break;
+      }
+      else{
+        this.savedCheckedData['groupCritical']=true
+      }
+    }
+    for(var i=0;i< ErrorKeys.length;i++)
+    {
+      if(this.savedCheckedData['ERROR'][ErrorKeys[i]]==false)
+      {
+        this.savedCheckedData['groupError']=false
+        break;
+      }
+      else{
+        this.savedCheckedData['groupError']=true
+      }
+    }
+    for(var i=0;i< WarningKeys.length;i++)
+    {
+      if(this.savedCheckedData['Warn'][WarningKeys[i]]==false)
+      {
+        this.savedCheckedData['groupWarn']=false
+        break;
+      }
+      else{
+        this.savedCheckedData['groupWarn']=true
+      }
+    }
+    
+    
   }
 
 
   ngOnInit(): void {
     
+
   }
   onCheckBoxClicked(){
     this.addLogData.emit(this.dynamicForm.value);
-    console.log(this.savedCheckedData)
-    console.log(this.dynamicForm.value)
+    this.savedCheckedData=this.dynamicForm.value
+    var criticalKeys = Object.keys(this.savedCheckedData.Critical)
+    var ErrorKeys = Object.keys(this.savedCheckedData.ERROR)
+    var WarningKeys = Object.keys(this.savedCheckedData.Warn)
+    
+    
+    for(var i=0;i< criticalKeys.length;i++)
+    {
+      if(this.savedCheckedData['Critical'][criticalKeys[i]]==false || this.savedCheckedData['Critical'][criticalKeys[i]]==undefined)
+      {
+        this.savedCheckedData['groupCritical']=false
+        break;
+      }
+      else{
+        this.savedCheckedData['groupCritical']=true
+      }
+    }
+    for(var i=0;i< ErrorKeys.length;i++)
+    {
+      if(this.savedCheckedData['ERROR'][ErrorKeys[i]]==false || this.savedCheckedData['ERROR'][ErrorKeys[i]]==undefined)
+      {
+        this.savedCheckedData['groupError']=false
+        break;
+      }
+      else{
+        this.savedCheckedData['groupError']=true
+      }
+    }
+    for(var i=0;i< WarningKeys.length;i++)
+    {
+      if(this.savedCheckedData['Warn'][WarningKeys[i]]==false || this.savedCheckedData['Warn'][WarningKeys[i]]==undefined)
+      {
+        this.savedCheckedData['groupWarn']=false
+        break;
+      }
+      else{
+        this.savedCheckedData['groupWarn']=true
+      }
+    }
+    
   
     
   }
@@ -131,6 +207,10 @@ export class CorrelationformdetailsComponent implements OnInit,OnChanges {
       }
       this.addLogData.emit(this.savedCheckedData);
     }
+  }
+  
+  checkSelectAllState(recentCheckedData){
+    
   }
   
 
