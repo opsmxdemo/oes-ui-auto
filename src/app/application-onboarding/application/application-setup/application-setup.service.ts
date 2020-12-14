f54560c8662cf7ead751e1447599b7180d2035b6
Satya Kiran <satya.rayasam@opsmx.io>
2020-12-14 15:55:45 +0530

    app basic setup

--- src/app/application-onboarding/application/application-setup/application-setup.service.ts
+++ src/app/application-onboarding/application/application-setup/application-setup.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AppConfigService } from 'src/app/services/app-config.service';
 
@@ -6,10 +7,25 @@ import { AppConfigService } from 'src/app/services/app-config.service';
 })
 export class ApplicationSetupService {
 
-  applicationDetails: any = {};
+  public static id: number = 0;
+  public static applicationDetails: any = {};
   showApplicationDetailsComp: boolean = true;
   showServiceDetailsComp: boolean;
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  init() {
+    this.getAppDetails();
+  }
+
+  getAppDetails() {
+    // call get app details api function this.getApplications() if id > 0
+    // else assign an empty object same as the get api response
+    // assign it to applicationDetails variable
+  }
+
+  getApplications() {
+    // call api with the this.id
+  }
 
 }
